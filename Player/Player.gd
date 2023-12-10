c15c4c55c94c9ab0991f2a783727afd42ce63e13
camgonmad <Camgonmad@gmail.com>
2023-12-10 05:08:49 -0500

    commented player code
    added player code comments and will wait for your input on further changes.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,33 +1,43 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Gets the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var doublej = false
 
 @onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Adds the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	# Resets double jump.
+	if is_on_floor():
+		doublej = false
 
-	# Handle jump.
+	# Handles jump input and player jump animation.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
+	elif Input.is_action_just_pressed("ui_accept") and not is_on_floor() and doublej == false:
+		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
+		doublej = true
 
-	# Get the input direction and handle the movement/deceleration.
+	# Gets the input direction and handles the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	print(direction)
+	
+	# Flips sprite sheet based on direction.
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 	
+	# Plays animations for player movement.
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
