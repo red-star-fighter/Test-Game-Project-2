30e592918629f3d1ba7469e81b2bff24fb8ee34a
Redstarfighter <westlyroots@gmail.com>
2023-12-11 17:37:46 -0500

    Update Label.gd

--- Player/Label.gd
+++ Player/Label.gd
@@ -6,7 +6,6 @@ var score = ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.text = "0"
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
