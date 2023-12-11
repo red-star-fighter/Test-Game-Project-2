9b0d88e799a4c337c11ff9c4e261edcaf918aeb0
camgonmad <Camgonmad@gmail.com>
2023-12-11 17:57:58 -0500

    consolidated code space
    removed empty lines for style preference

--- Player/Label.gd
+++ Player/Label.gd
@@ -7,11 +7,9 @@ var score = ""
 func _ready():
 	self.text = "SCORE: 0"
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# Checks player score and updates max score.
 	if player.global_transform.origin.y > int(score):
 		score = str(int(player.global_transform.origin.y))
 		self.text = "SCORE: " + score
-
