18fe721548c757a2e58c68fa041c96e2c44b416c
camgonmad <Camgonmad@gmail.com>
2023-12-11 01:30:05 -0500

    score system
    created player scoring system and labels

---
+++ Player/Label.gd
@@ -0,0 +1,18 @@
+extends Label
+
+@onready var player = get_node("../CollisionShape2D")
+var score = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.text = "0"
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	# Checks player score and updates max score.
+	if player.global_transform.origin.y > int(score):
+		score = str(int(player.global_transform.origin.y))
+		self.text = score
+	pass
--- Player/Player.gd
+++ Player/Player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	# Resets double jump.
 	if is_on_floor():
 		doublej = false
-
+	
 	# Handles jump input and player jump animation.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
